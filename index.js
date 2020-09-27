af94f883790bca38f63ec86804001c4c8866253d
wellpinho <wellpinho@outlook.com>
2020-09-27 20:35:32 -0300

    Criado a estrutura do projeto

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({}))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  res.status(200).render('index')
+})
+
+const port = process.env.PORT || 8080
+app.listen(port, () => {
+  console.log(`Running on port http://localhost:${port}`)
+})
